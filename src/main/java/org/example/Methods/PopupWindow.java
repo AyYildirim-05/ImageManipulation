c506b2c3d165aeff41cb664cdfc82d196be83a65
Ahmet Yusuf Yildirim <140295077+AyYildirim-05@users.noreply.github.com>
2025-08-04 22:26:32 -0400

    final commit for now. finished base structure of the application.

--- src/main/java/org/example/Methods/PopupWindow.java
+++ src/main/java/org/example/Methods/PopupWindow.java
@@ -1,6 +1,48 @@
 package org.example.Methods;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class PopupWindow {
+    /**
+     * Displays a modal alert box with the given title and message
+     *
+     * @param title   The title of the alert box
+     * @param message The message to be displayed in the alert box
+     */
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+
+        // Set the modality of the window to prevent interaction with other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        // Create a label and button for the alert box
+        Label label = new Label();
+        label.setId("alertLabel");
+        label.setText(message);
+        Button closeButton = new Button("Close");
+        closeButton.setId("alertButton");
+        closeButton.setOnAction(event -> window.close());
+
+        // Create a layout to arrange the label and button
+        VBox layout = new VBox(10);
+        layout.setId("alertLayout");
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        // Set the scene with the layout
+        Scene scene = new Scene(layout);
+
+        // Set the scene and show the window
+        window.setScene(scene);
+        window.showAndWait();
+    }
 
 }
-
--- src/main/java/org/example/Methods/UserInterface.java
+++ src/main/java/org/example/Methods/UserInterface.java
@@ -1,43 +1,53 @@
 package org.example.Methods;
 
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class UserInterface {
 
-    @FXML
-    private RadioButton toGrayScale1, toGrayScale2, blurGrayScale, blurColor, heavyBlurGrayScale, heavyBlurColor, edgeDetection;
-    @FXML
-    private RadioButton pixelateGrayScale, pixelateColor, resize, brighten;
-
+    @FXML private RadioButton toGrayScale1, toGrayScale2, blurGrayScale, blurColor, heavyBlurGrayScale, heavyBlurColor, edgeDetection;
+    @FXML private RadioButton pixelateGrayScale, pixelateColor, resize, brighten;
     @FXML private Button importButton, exportButton, applyButton;
     @FXML private Tab singularTab, multipleTab;
-    @FXML
-    private TabPane tabPane;
+    @FXML private TabPane tabPane;
     @FXML private ImageView imageView;
-    @FXML
-    private TextField numberField;
+    @FXML private TextField numberField;
 
     private String fileName;
     private Stage stage;
-    private BufferedImage image;
-    private BufferedImage applied;
+    private BufferedImage image, applied;
     private FileMethods fm;
     private Plugins plugin;
-
     private ToggleGroup group1, group2;
     private Image display;
 
     @FXML
     private void initialize() {
+        setupFunction();
+
+        importButton.setOnAction(event -> importFunction());
+
+        applyButton.setOnAction(event -> applyFunction());
+
+        exportButton.setOnAction(event -> exportFunction());
+    }
+
+    /**
+     * Method that takes care of the setup of the main program.
+     */
+    private void setupFunction() {
         if (fm == null && plugin == null) {
             fm = new FileMethods();
             plugin = new Plugins();
@@ -59,121 +69,151 @@ public class UserInterface {
             resize.setToggleGroup(group2);
             brighten.setToggleGroup(group2);
         }
-
-        importButton.setOnAction(event -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Import Image File");
-            fileChooser.getExtensionFilters().add(
-                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
-            );
-
-            File selectedFile = fileChooser.showOpenDialog(importButton.getScene().getWindow());
-
-            if (selectedFile != null) {
-                String filePath = selectedFile.getAbsolutePath();
-                System.out.println("Selected file path: " + filePath);
-                image = fm.loadImage(filePath);
-
-                if (image != null) {
-                    display = fm.toImage(image);
-                    imageView.setFitWidth(display.getWidth());
-                    imageView.setFitHeight(display.getHeight());
-                    imageView.setImage(display);
-
-
-                } else {
-                    System.out.println("⚠️ Failed to load image.");
-                }
-
-            } else {
-                System.out.println("File selection cancelled.");
-            }
-        });
-
-        applyButton.setOnAction(event -> {
-            Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
-
-            switch (selectedTab.getId()) {
-                case "singularTab":
-                    RadioButton selectedRadioButton1 = (RadioButton) group1.getSelectedToggle();
-
-                    switch (selectedRadioButton1.getId()) {
-                        case "toGrayScale1":
-                            applied = plugin.toGrayScale(image);
-                            break;
-                        case "toGrayScale2":
-                            applied = plugin.toGrayScale2(image);
-                            break;
-                        case "blurGrayScale":
-                            applied = plugin.blur(image);
-                            break;
-                        case "blurColor":
-                            applied = plugin.blurColor(image);
-                            break;
-                        case "heavyBlurGrayScale":
-                            applied = plugin.heavyBlur(image);
-                            break;
-                        case "heavyBlurColor":
-                            applied = plugin.heavyBlurColor(image);
-                            break;
-                        case "edgeDetection":
-                            applied = plugin.detectEdges(image);
-                            break;
-                        default:
-                            System.out.println("error");
-                            return;
-                    }
-
-                    display = fm.toImage(applied);
-                    imageView.setFitWidth(display.getWidth());
-                    imageView.setFitHeight(display.getHeight());
-                    imageView.setImage(display);
-
-//                    image = applied;
-
-                    break;
-                case "multipleTab":
-                    getText();
-                    RadioButton selectedRadioButton2 = (RadioButton) group2.getSelectedToggle();
-                    switch (selectedRadioButton2.getId()) {
-                        case "pixelateGrayScale":
-                            applied = plugin.pixelateGreyScale(image, Integer.parseInt(numberField.getText()));
-                            break;
-                        case "pixelateColor":
-                            applied = plugin.pixelateColor(image, Integer.parseInt(numberField.getText()));
-                            break;
-                        case "resize":
-                            applied = plugin.resize(image, Integer.parseInt(numberField.getText()));
-                            break;
-                        case "brighten":
-                            applied = plugin.brighten(image, Integer.parseInt(numberField.getText()));
-                            break;
-                        default:
-                            System.out.println("error");
-                            return;
-                    }
-
-                    display = fm.toImage(applied);
-                    imageView.setFitWidth(display.getWidth());
-                    imageView.setFitHeight(display.getHeight());
-                    imageView.setImage(display);
-
-//                    image = applied;
-
-                    break;
-                default:
-                    System.out.println("Unknown tab.");
-                    break;
-            }
-        });
-
-        exportButton.setOnAction(event -> {
-
-        });
-
     }
 
+    /**
+     * Method that takes care of the importing files to the main project.
+     */
+    private void importFunction() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Import Image File");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+        );
 
+        File selectedFile = fileChooser.showOpenDialog(importButton.getScene().getWindow());
+
+        if (selectedFile != null) {
+            String filePath = selectedFile.getAbsolutePath();
+            System.out.println("Selected file path: " + filePath);
+
+            image = fm.loadImage(filePath);
+
+            if (image != null) {
+                display = fm.toImage(image);
+                imageView.setFitWidth(display.getWidth());
+                imageView.setFitHeight(display.getHeight());
+                imageView.setImage(display);
+
+
+            } else {
+                PopupWindow.display("Input Error", "⚠️ Failed to load image.");
+            }
+
+        } else {
+            PopupWindow.display("Input Cancelled", "File selection cancelled.");
+        }
+    }
+
+    /**
+     * Method that takes care of applying image filters depending on the user's choices.
+     */
+    private void applyFunction() {
+        Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
+
+        switch (selectedTab.getId()) {
+            case "singularTab":
+                RadioButton selectedRadioButton1 = (RadioButton) group1.getSelectedToggle();
+
+                switch (selectedRadioButton1.getId()) {
+                    case "toGrayScale1":
+                        applied = plugin.toGrayScale(image);
+                        break;
+                    case "toGrayScale2":
+                        applied = plugin.toGrayScale2(image);
+                        break;
+                    case "blurGrayScale":
+                        applied = plugin.blur(image);
+                        break;
+                    case "blurColor":
+                        applied = plugin.blurColor(image);
+                        break;
+                    case "heavyBlurGrayScale":
+                        applied = plugin.heavyBlur(image);
+                        break;
+                    case "heavyBlurColor":
+                        applied = plugin.heavyBlurColor(image);
+                        break;
+                    case "edgeDetection":
+                        applied = plugin.detectEdges(image);
+                        break;
+                    default:
+                        PopupWindow.display("No chosen option","There is no effect to apply on the image.");
+                        return;
+                }
+
+                display = fm.toImage(applied);
+                imageView.setFitWidth(display.getWidth());
+                imageView.setFitHeight(display.getHeight());
+                imageView.setImage(display);
+
+                image = applied;
+
+                break;
+            case "multipleTab":
+                getText();
+                RadioButton selectedRadioButton2 = (RadioButton) group2.getSelectedToggle();
+                switch (selectedRadioButton2.getId()) {
+                    case "pixelateGrayScale":
+                        applied = plugin.pixelateGreyScale(image, Integer.parseInt(numberField.getText()));
+                        break;
+                    case "pixelateColor":
+                        applied = plugin.pixelateColor(image, Integer.parseInt(numberField.getText()));
+                        break;
+                    case "resize":
+                        applied = plugin.resize(image, Integer.parseInt(numberField.getText()));
+                        break;
+                    case "brighten":
+                        applied = plugin.brighten(image, Integer.parseInt(numberField.getText()));
+                        break;
+                    default:
+                        PopupWindow.display("No chosen option","There is no effect to apply on the image.");
+                        return;
+                }
+
+                display = fm.toImage(applied);
+                imageView.setFitWidth(display.getWidth());
+                imageView.setFitHeight(display.getHeight());
+                imageView.setImage(display);
+
+                image = applied;
+
+                break;
+            default:
+                PopupWindow.display("Unknown tab","There is no effect to know effect to apply on the image.");
+                break;
+        }
+    }
+
+    /**
+     * Method that takes care of exporting the user's modifications to the image.
+     */
+    private void exportFunction() {
+        WritableImage fxImage = (WritableImage) fm.toImage(applied);
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Image File");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png")
+        );
+
+        File file = fileChooser.showSaveDialog(stage);
+        if (file != null) {
+            try {
+                BufferedImage bImage = SwingFXUtils.fromFXImage(fxImage, null);
+
+                ImageIO.write(bImage, "png", file);
+                System.out.println("Saved to: " + file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Function that limits the entry of the TextField.
+     * Method only allows digits to be entered to the TextField.
+     */
     private void getText() {
         numberField.textProperty().addListener((obs, oldVal, newVal) -> {
             if (!newVal.matches("\\d*(\\.\\d*)?")) {
